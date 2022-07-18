390ab57b9b6494d11a1ebdf72d304149a139dbb4
shii <shii@shii-K53SV>
2022-07-18 08:50:04 +0300

    First java commit

---
+++ Shii/src/tuna.java
@@ -0,0 +1,12 @@
+
+public class tuna {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double shee = 3.56;
+		System.out.println(shee);
+		System.out.print("\\\r");
+
+	}
+
+}
